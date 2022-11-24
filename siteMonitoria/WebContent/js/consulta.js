869b8db003011da65c59b47b87565bb9c6c9a485
MATHEUS VICENZI <matheus_vicenzi@estudante.sc.senai.br>
2022-11-23 22:02:21 -0300

    23/11/2022

--- siteMonitoria/WebContent/js/consulta.js
+++ siteMonitoria/WebContent/js/consulta.js
@@ -21,6 +21,121 @@ function validaConsulta(){
 	}
 }
 
+function habilitaCampoFiltro(valorFiltro){
+	var textoLabel
+	boxFiltro = document.getElementById("boxFiltro");
+	boxFiltro.innerHTML = "";
+	if(valorFiltro==="aluno" || valorFiltro==="obs"){
+		if(valorFiltro==="aluno"){
+			var id="aluno";
+			var textoCampo="Aluno"
+			var nameCampo="txtaluno"
+		}else if(valorFiltro === "obs"){
+			var id="obs";
+			var textoCampo="Observação"
+			var nameCampo="txtobs"
+		}
+		console.log(valorFiltro)
+		
+		textoLabel = document.createTextNode(textoCampo);
+		
+		labelFiltro = document.createElement("label");
+		labelFiltro.setAttribute("for", id);
+		$(labelFiltro).addClass("form-label");
+		
+		labelFiltro.appendChild(textoLabel);
+		
+		var campoAluno = document.createElement("input");
+		$(campoAluno).addClass("form-control");
+		campoAluno.setAttribute("id", id);
+		campoAluno.setAttribute("name", nameCampo);
+		campoAluno.setAttribute("type", "text");
+		
+		boxFiltro.appendChild(labelFiltro);
+		boxFiltro.appendChild(campoAluno);
+		
+	}else if(valorFiltro === "turma"){
+		
+		labelFiltro = document.createElement("label");
+		labelFiltro.setAttribute("for", "turma");
+		$(labelFiltro).addClass("form-label");
+		
+		labelFiltro.appendChild(document.createTextNode("Turma"))
+		
+		var campoTurma = document.createElement("select");
+		$(campoTurma).addClass("form-control");
+		$(campoTurma).addClass("form-select");
+		
+		var valorPadrao = document.createElement("option")
+		valorPadrao.setAttribute("value", "")
+		valorPadrao.appendChild(document.createTextNode("Selecione"))
+		
+		var valorTurma1 = document.createElement("option");
+		valorTurma1.setAttribute("value", "1")
+		valorTurma1.appendChild(document.createTextNode("1º Ano"))
+		
+		var valorTurma2 = document.createElement("option");
+		valorTurma2.setAttribute("value", "2")
+		valorTurma2.appendChild(document.createTextNode("2º Ano"))
+		
+		var valorTurma3 = document.createElement("option");
+		valorTurma3.setAttribute("value", "3");
+		valorTurma3.appendChild(document.createTextNode("3º Ano"));
+		
+		campoTurma.appendChild(valorPadrao);
+		campoTurma.appendChild(valorTurma1);
+		campoTurma.appendChild(valorTurma2);
+		campoTurma.appendChild(valorTurma3);
+		
+		boxFiltro.appendChild(labelFiltro);
+		boxFiltro.appendChild(campoTurma);
+		
+	}else if(valorFiltro === "data"){
+		
+		var dataInicioBox = document.createElement("div");
+		$(dataInicioBox).addClass("form-input")
+		
+		var dataFimBox = document.createElement("div");
+		$(dataFimBox).addClass("form-input")
+		
+		//Label data Inicio
+		labelFiltroDataInicio = document.createElement("label");
+		labelFiltroDataInicio.setAttribute("for", "dataInicio");
+		$(labelFiltroDataInicio).addClass("form-label");
+		labelFiltroDataInicio.appendChild(document.createTextNode("Data Inicio"))
+		
+		//Label data Fim
+		labelFiltroDataFim = document.createElement("label");
+		labelFiltroDataFim.setAttribute("for", "dataInicio");
+		$(labelFiltroDataFim).addClass("form-label");
+		labelFiltroDataFim.appendChild(document.createTextNode("Data Fim"));
+		
+		var campoDataInicio = document.createElement("input");
+		$(campoDataInicio).addClass("form-control");
+		campoDataInicio.setAttribute("type", "date");
+		campoDataInicio.setAttribute("id", "dataInicio");
+		campoDataInicio.setAttribute("name", "txtdatainicio");
+		
+		var campoDataFim = document.createElement("input");
+		$(campoDataFim).addClass("form-control");
+		campoDataFim.setAttribute("type", "date");
+		campoDataFim.setAttribute("id", "dataFim");
+		campoDataFim.setAttribute("name", "txtdatafim");
+		
+		dataInicioBox.appendChild(labelFiltroDataInicio);
+		dataInicioBox.appendChild(campoDataInicio);
+		
+		dataFimBox.appendChild(labelFiltroDataFim);
+		dataFimBox.appendChild(campoDataFim);
+		
+		boxFiltro.appendChild(dataInicioBox);
+		boxFiltro.appendChild(dataFimBox);
+		
+	}//inserir validação para Status
+	
+	
+}
+
 function buscarMonitorias(){
 	
 	if(!validaConsulta()){
